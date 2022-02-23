449f0bab1d69d3f5ff00ddc5f4ac3911b88b3f0d
Clodoaldo Souza <clodoaldosouza@hotmail.com>
2022-02-23 14:21:11 -0300

    Add examples with Arrays, Slices, Maps

--- src/arrays/arrays.go
+++ src/arrays/arrays.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	simpleArray()
+	slice()
+	simpleMap()
+}
+
+func simpleArray() {
+	myArray := [2]int{1, 2}
+	fmt.Println(myArray)
+}
+
+func slice() {
+	slice := make([]int, 2)
+	slice[0] = 1
+	slice[1] = 2
+	slice = append(slice, 3, 4, 5)
+	fmt.Println(slice)
+}
+
+func simpleMap() {
+	myMap := make(map[string]int)
+	myMap["Hello"] = 10
+	myMap["World"] = 20
+	fmt.Println(myMap["Hello"])
 }

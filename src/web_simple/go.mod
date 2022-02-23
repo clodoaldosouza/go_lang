5126fe827c1b1e46386754ad6f83496e74888ddb
Clodoaldo Souza <clodoaldosouza@hotmail.com>
2022-02-23 14:20:11 -0300

    Add modifications in Web Server Simple example

---
+++ src/web_simple/go.mod
@@ -0,0 +1,3 @@
+module web_simple
+
+go 1.17
--- src/web_simple/simple.go
+++ src/web_simple/simple.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+type Test struct {
+	Name string
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome!!!")
 }
 
+func Index(w http.ResponseWriter, r *http.Request) {
+	test := Test{
+		"Hello World Server",
+	}
+	myTemplate := template.Must(template.ParseFiles("src/web_simple/index.html"))
+	myTemplate.Execute(w, test)
+
+	myJson, err := json.Marshal(test)
+	if err != nil {
+		panic("Fail to generate Json")
+	}
+	fmt.Println("Json : " + string(myJson))
+}
+
 func HandleRequest() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/home", Index)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 

716febcb3cb54efcf509511eea12192adcc98166
Clodoaldo Souza <clodoaldosouza@hotmail.com>
2022-02-23 14:22:34 -0300

    Add examples with variables and types

--- src/print_types/print_types.go
+++ src/print_types/print_types.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	myName := "My Name"
+	myAge := 20
+	myBill := (5.0 / 1025)
+	myBoolean := false
+
+	fmt.Printf("Value = `%v` ", myName)
+	fmt.Printf("Type = `%T` \n", myName)
+
+	fmt.Printf("Value = `%v` ", myAge)
+	fmt.Printf("Type = `%T` \n", myAge)
+
+	fmt.Printf("Value = `%v` ", myBill)
+	fmt.Printf("Type = `%T` \n", myBill)
+
+	fmt.Printf("Value = `%v` ", myBoolean)
+	fmt.Printf("Type = `%T` \n", myBoolean)
 
 }

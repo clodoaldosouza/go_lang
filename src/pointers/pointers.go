65eca482da06bf538b754ec45c6084956b41f5af
Clodoaldo Souza <clodoaldosouza@hotmail.com>
2022-02-23 14:21:47 -0300

    Add examples with pointers

--- src/pointers/pointers.go
+++ src/pointers/pointers.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	x := 123
+	y := &x
+	var z *int = &x
+	var w = myPointer(&x)
+
+	fmt.Println(&x) // memory address
+	fmt.Println(*y) // value of memory
+	fmt.Println(z)  // memory address
+	fmt.Println(*z) // value of memory
+	fmt.Println(w)  // value of memory
+
+}
+
+func myPointer(i *int) int {
+	return *i
 }
